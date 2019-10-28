11a270e0ab88ce1ca546409f1511c77afe9446fc
Nasir <nasirzid@users.noreply.github.com>
2019-10-28 18:37:49 +0500

    Update the version from 5.1 to 3.6

--- plugins/wp-file-manager/file_folder_manager.php
+++ plugins/wp-file-manager/file_folder_manager.php
@@ -4,7 +4,7 @@
   Plugin URI: https://wordpress.org/plugins/wp-file-manager
   Description: Manage your WP files.
   Author: mndpsingh287
-  Version: 5.1
+  Version: 3.6
   Author URI: https://profiles.wordpress.org/mndpsingh287
   License: GPLv2
  **/
